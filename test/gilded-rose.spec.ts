b8831e53b8e7b7f183daabc53a942fe0e931f04d
Oli Goodall <Oli.Goodall@softwire.com>
2022-08-18 12:30:24 +0100

    Experiment test working

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,18 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+    // it('should foo', function() {
+    //     const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    //     const items = gildedRose.updateQuality();
+    //     expect(items[0].name).to.equal('foo');
+    // });
+
+    it('experiment to see what the expected output is', function(){
+        const gildedRose = new GildedRose([ new Item('Normal Item', 1, 10)]);
+        gildedRose.updateQuality();
+        let result = gildedRose.updateQuality();
+        expect(result[0].sellIn).to.equal(-1);
+        expect(result[0].quality).to.equal(7);
+    })
 
 });
